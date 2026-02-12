29ce19c4a372fb392d3d4c2bb9713d643f6a6a9e
Azuma9598 <19025@nbr.ac.th>
2026-02-12 14:31:29 +0700

    middlewares/errorHandler.js

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,25 @@
+// src/middlewares/errorHandler.js
+const { AppError } = require('../utils/errors');
+const logger = require('../utils/logger');
+
+module.exports = (err, req, res, next) => {
+  let { statusCode = 500, message = 'Something went wrong' } = err;
+
+  // Handle multer errors
+  if (err.code === 'FILE_TOO_LARGE') {
+    statusCode = 413;
+    message = `File too large. Max size: ${process.env.MAX_FILE_SIZE || '100MB'}`;
+  } else if (err.name === 'MulterError') {
+    statusCode = 400;
+    message = err.message;
+  }
+
+  // Log error
+  logger.error(`${statusCode} - ${message}`, err);
+
+  res.status(statusCode).json({
+    success: false,
+    error: message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  });
+};
