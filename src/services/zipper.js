02a6ff09d3cf16a87e7635d205beb386f9828228
Azuma9598 <19025@nbr.ac.th>
2026-02-12 14:27:13 +0700

    zipper.js

---
+++ src/services/zipper.js
@@ -0,0 +1,28 @@
+// src/services/zipper.js
+const archiver = require('archiver');
+const { PassThrough } = require('stream');
+const { BadRequestError } = require('../utils/errors');
+const logger = require('../utils/logger');
+
+/**
+ * Create a zip stream from a directory.
+ * @param {string} sourceDir - Directory to zip
+ * @returns {PassThrough} Stream of zip data
+ */
+function zipDirectory(sourceDir) {
+  const archive = archiver('zip', { zlib: { level: 9 } });
+  const stream = new PassThrough();
+
+  archive.on('error', (err) => {
+    logger.error('Archive error:', err);
+    stream.destroy(err);
+  });
+
+  archive.pipe(stream);
+  archive.directory(sourceDir, false);
+  archive.finalize();
+
+  return stream;
+}
+
+module.exports = { zipDirectory };
