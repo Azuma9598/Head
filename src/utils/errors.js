4a2aa8e8f8ea3b6284a0bbc3a3a9f0440a13f68f
Azuma9598 <19025@nbr.ac.th>
2026-02-12 14:14:56 +0700

    errors.js

---
+++ src/utils/errors.js
@@ -0,0 +1,27 @@
+// src/utils/errors.js
+class AppError extends Error {
+  constructor(message, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class BadRequestError extends AppError {
+  constructor(message = 'Bad request') {
+    super(message, 400);
+  }
+}
+
+class InternalServerError extends AppError {
+  constructor(message = 'Internal server error') {
+    super(message, 500);
+  }
+}
+
+module.exports = {
+  AppError,
+  BadRequestError,
+  InternalServerError
+};
